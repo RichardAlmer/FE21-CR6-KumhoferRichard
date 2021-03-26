39de54798ca7c9bb1351b051f55da56a4b2891ca
Richard Kumhofer <richard.kumhofer@gmail.com>
2021-03-26 10:18:11 +0100

    started CR6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { TravelsComponent } from './travels/travels.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    HomeComponent,
+    BlogComponent,
+    TravelsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
